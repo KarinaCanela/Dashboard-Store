d867d6f0e147c453fbea6874ba5d95c62e7c13d7
KarinaCanela <kcanelacobos@gmail.com>
2024-11-06 17:52:25 -0600

    Se agregaron imagenes para la pantalla principal y se inicio la seccion de Orders

--- src/App.jsx
+++ src/App.jsx
@@ -29,10 +29,10 @@ function App() {
         <button className="text-white p-2" onClick={toggleMenu}>
           {showMenu ? <RiCloseLine/> : <RiMenu2Line/>} </button>
       </nav>
-      <main className="lg:pl-28 grid grid-cols-1 lg:grid-cols-8">
-        <div className="lg:col-span-6 ">
-          <header className="p-4">
-            <div className="flex flex-col gap-4 mb-6">
+      <main className="lg:pl-32 grid grid-cols-1 lg:grid-cols-8 p-4 pb-20">
+        <div className="lg:col-span-6 md:p-8">
+          <header className="">
+            <div className="flex flex-col md:flex-row  md:justify-between md:items-center gap-4 mb-6">
               <div>
                 <h1 className="text-2xl text-gray-300">Jeager Resto</h1>
                 <p className="text-gray-500">4 noviembre 2024</p>
@@ -45,23 +45,109 @@ function App() {
                 </div>
               </form>
             </div>
-            <nav className="text-gray-300 flex items-center justify-between border-b mb-6">
+            <nav className="text-gray-300 flex items-center justify-between md:justify-start md:gap-8 border-b mb-6">
               <a href="#" className="relative py-2 pr-4 before:w-1/2 before:h-[2px] before:absolute
               before:bg-[#ec7c6a] before:left-0 before:rounded-full before:bottom-[-1px] text-[#ec7c6a]">Hot dishes</a>
               <a href="#" className="py-2 pr-4">Cold dishes</a>
               <a href="#" className="py-2 pr-4">Soup</a>
               <a href="#" className="py-2">Grill</a>
             </nav>
-            <div>
-              <h2 className="text-xl text-gray-300">Choose Dishes</h2>
-              <button className="flex items-center gap-4 text-gray-300">
-                <RiArrowDownSLine/>
-                Dine in
-              </button>
-            </div>
           </header>
+          <div className="flex items-center justify-between mb-9">
+            <h2 className="text-xl text-gray-300">Choose Dishes</h2>
+            <button className="flex items-center gap-4 text-gray-300 bg-[#1F1D2B] py-2 px-4 rounded-lg">
+              <RiArrowDownSLine/>
+              Dine in
+            </button>
+          </div>
+          <div className="p-8 grid grid-cols-1 gap-14 md:grid-cols-2 lg:grid-cols-3">
+            <div className="bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center text-gray-300
+            text-center gap-2">
+              <img src="bacalao_planchabg.png" className="w-55 h-55 object-cover -mt-20 shadow-2xl
+              rounded-full" alt="" />
+              <p className="text-xl">Bacalao con verduras a la plancha</p>
+              <span className="text-gray-400">$180.00</span>
+              <p className="text-gray-600">20 Platillos disponibles</p>
+            </div>
+            <div className="bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center text-gray-300
+            text-center gap-2">
+              <img src="cangrejo_currybg.png" className="w-55 h-55 object-cover -mt-20 shadow-2xl
+              rounded-full" alt="" />
+              <p className="text-xl">Crema de cangrejo con curry</p>
+              <span className="text-gray-400">$260.00</span>
+              <p className="text-gray-600">10 Platillos disponibles</p>
+            </div>
+            <div className="bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center text-gray-300
+            text-center gap-2">
+              <img src="empanadasbg.png" className="w-55 h-55 object-cover -mt-20 shadow-2xl
+              rounded-full" alt="" />
+              <p className="text-xl">Empanadas mixtas</p>
+              <span className="text-gray-400">$80.00</span>
+              <p className="text-gray-600">30 Platillos disponibles</p>
+            </div>
+            <div className="bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center text-gray-300
+            text-center gap-2">
+              <img src="hamburguesabg.png" className="w-55 h-55 object-cover -mt-20 shadow-2xl
+              rounded-full" alt="" />
+              <p className="text-xl">Hamburguesa con gajos de papa</p>
+              <span className="text-gray-400">$150.00</span>
+              <p className="text-gray-600">25 Platillos disponibles</p>
+            </div>
+            <div className="bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center text-gray-300
+            text-center gap-2">
+              <img src="ketobg.png" className="w-55 h-55 object-cover -mt-20 shadow-2xl
+              rounded-full" alt="" />
+              <p className="text-xl">Desayuno Keto</p>
+              <span className="text-gray-400">$190.00</span>
+              <p className="text-gray-600">25 Platillos disponibles</p>
+            </div>
+            <div className="bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center text-gray-300
+            text-center gap-2">
+              <img src="pastabg.png" className="w-55 h-55 object-cover -mt-20 shadow-2xl
+              rounded-full" alt="" />
+              <p className="text-xl">Pasta con camarones</p>
+              <span className="text-gray-400">$180.00</span>
+              <p className="text-gray-600">20 Platillos disponibles</p>
+            </div>
+            <div className="bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center text-gray-300
+            text-center gap-2">
+              <img src="pollo_ensaladabg.png" className="w-55 h-55 object-cover -mt-20 shadow-2xl
+              rounded-full" alt="" />
+              <p className="text-xl">Pollo a la plancha con ensalada</p>
+              <span className="text-gray-400">$160.00</span>
+              <p className="text-gray-600">20 Platillos disponibles</p>
+            </div>
+            <div className="bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center text-gray-300
+            text-center gap-2">
+              <img src="tacos_camaronbg.png" className="w-55 h-55 object-cover -mt-20 shadow-2xl
+              rounded-full" alt="" />
+              <p className="text-xl">Tacos de camaron</p>
+              <span className="text-gray-400">$200.00</span>
+              <p className="text-gray-600">20 Platillos disponibles</p>
+            </div>
+            <div className="bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center text-gray-300
+            text-center gap-2">
+              <img src="tortilla_espanolabg.png" className="w-55 h-55 object-cover -mt-20 shadow-2xl
+              rounded-full" alt="" />
+              <p className="text-xl">Tortilla española</p>
+              <span className="text-gray-400">$150.00</span>
+              <p className="text-gray-600">25 Platillos disponibles</p>
+            </div>
+          </div>
         </div>
-        <div className="lg:col-span-2 fixed lg:static right-0">Carrito</div>
+        <div className="lg:col-span-2 fixed lg:static right-0 top-0 bg-[#1F1D2B] w-full h-full ">
+          {/* Orders */}
+          <div className="relative pt-16 text-gray-300 p-8">
+            <RiCloseLine className="absolute left-4 top-4 p-3 box-content text-gray-300 bg-[#262837]
+            rounded-full text-xl"/>
+            <h1 className="text-2xl my-4">Orden #1234</h1>
+            <div className="flex items-center gap-4 flex-wrap">
+              <button className="bg-[#ec7c6a] text-white py-2 px-4 rounded-xl">Dine in</button>
+              <button className="text-[#ec7c6a] border border-gray-500 py-2 px-4 rounded-xl">Dine in</button>
+              <button className="text-[#ec7c6a] border border-gray-500 py-2 px-4 rounded-xl">Dine in</button>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   )
